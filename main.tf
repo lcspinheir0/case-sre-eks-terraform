9b308f62c3f69ecd069f65c8f7fa0067acf64a77
lucas <lucas>
2025-07-12 15:56:09 -0300

    feat: criar e associar route tables públicas e privadas (alta disponibilidade)

--- main.tf
+++ main.tf
@@ -67,3 +67,48 @@ resource "aws_nat_gateway" "nat" {
     Ambiente = var.env
   }
 }
+
+# Route table Pública(e associação)
+# Garante que qualquer recurso lançado numa subnet pública (EC2, ELB, NAT Gateway etc.) tenha acesso à internet.
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.eks_vpc.id
+  tags = {
+    Name = "${var.project}-public-rt"
+    Ambiente = var.env
+  }
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.gw.id
+}
+
+resource "aws_route_table_association" "public" {
+  count = 2
+  subnet_id = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+# Route Table para subnets privadas (uma para cada AZ)
+resource "aws_route_table" "private" {
+  count  = 2
+  vpc_id = aws_vpc.eks_vpc.id
+  tags = {
+    Name = "${var.project}-private-rt-${element(var.azs, count.index)}"
+    Ambiente = var.env
+  }
+}
+
+resource "aws_route" "private_nat_access" {
+  count                  = 2
+  route_table_id         = aws_route_table.private[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat[count.index].id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = 2
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
